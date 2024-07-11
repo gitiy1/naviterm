c305bf0bc979a4632109b13c444be1801dbf8300
Jaime Pando Acedo <jaime@jpandoac.es>
2024-07-11 13:06:39 +0200

    ui: Add a recent albums list to the home page

--- src/ui/tab_home.rs
+++ src/ui/tab_home.rs
@@ -1,16 +1,43 @@
 use ratatui::Frame;
 use ratatui::layout::Rect;
-use ratatui::text::Line;
-use ratatui::widgets::{Block, Paragraph};
+use ratatui::style::Color::{Gray, Yellow};
+use ratatui::style::{Modifier, Style};
+use ratatui::text::{Line, Span, Text};
+use ratatui::widgets::{Block, Borders, List, ListItem, Paragraph};
 use crate::app::{App, AppResult};
 
 pub fn draw_tab(app: &mut App, area: Rect, frame: &mut Frame) -> AppResult<()> {
     
     let recent_albums = app.database.recent_albums();
-    
+
+    let block = Block::new()
+        .title(Line::raw("Recent albums").centered())
+        .borders(Borders::TOP);
+
     if recent_albums.is_empty() {
         frame.render_widget(Paragraph::new(
-            Line::from("No recent albums...")).block(Block::default()),area);
+            Line::from("No recent albums...")).block(Block::default()).block(block),area);
+    }
+    else {
+        let items = recent_albums.iter().enumerate()
+            .map(|(_i, album)| {
+                let album_item = Text::from(vec![
+                    Line::from(vec![
+                        Span { content: album.name().into(), style: Style::default().fg(Yellow).add_modifier(Modifier::BOLD) },
+                        Span { content: " from ".into(), style: Style::default().fg(Gray).add_modifier(Modifier::ITALIC) },
+                        Span { content: album.artist().into(), style: Style::default().fg(Gray).add_modifier(Modifier::ITALIC) },
+                    ]),
+                    Line::from(vec![
+                        Span { content: album.genre().into(), style: Style::default().fg(Gray).add_modifier(Modifier::ITALIC) },
+                        Span { content: ", ".into(), style: Style::default() },
+                        Span { content: album.song_count().into(), style: Style::default().fg(Gray).add_modifier(Modifier::ITALIC) },
+                        Span { content: " songs".into(), style: Style::default().fg(Gray).add_modifier(Modifier::ITALIC) },
+                    ])
+                ]);
+                ListItem::from(album_item)
+            });
+        let list = List::new(items).block(block);
+        frame.render_widget(list,area);
     }
 
     Ok(())
